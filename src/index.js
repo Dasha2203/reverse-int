beecf7787ad1108ebce7d58d857c436fa923dbd5
Darya Korbut <kitkatlolipop@mail.ru>
2020-10-06 11:44:44 +0300

    feat: add reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let result = String(n).split("");
+    if (result[0] === '-') {
+        result.shift();
+    }
+  return Number(result.reverse().join(""))
 }
